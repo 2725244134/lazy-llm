61640ac5d2a2c6c491084051a59a715c05c05bbf
cccc <2725244134@qq.com>
2026-02-08 00:38:19 +0800

    fix(ui): increase default zoom for sidebar and panes

--- electron/views/paneRuntimePreferences.ts
+++ electron/views/paneRuntimePreferences.ts
@@ -1,3 +1,3 @@
 export const PANE_ACCEPT_LANGUAGES = 'en-US,en,zh-CN,zh';
-export const PANE_DEFAULT_ZOOM_FACTOR = 1.1;
-export const SIDEBAR_DEFAULT_ZOOM_FACTOR = 1.0;
+export const PANE_DEFAULT_ZOOM_FACTOR = 1.2;
+export const SIDEBAR_DEFAULT_ZOOM_FACTOR = 1.1;
