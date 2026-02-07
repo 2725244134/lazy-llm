e913ca0a57baa482a7cb885e9574de638cf37e93
cccc <2725244134@qq.com>
2026-02-07 23:28:14 +0800

    fix(electron): normalize pane accept-language format

--- electron/views/paneRuntimePreferences.ts
+++ electron/views/paneRuntimePreferences.ts
@@ -1,4 +1,4 @@
-export const PANE_ACCEPT_LANGUAGES = 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7';
+export const PANE_ACCEPT_LANGUAGES = 'en-US,en,zh-CN,zh';
 export const PANE_DEFAULT_ZOOM_FACTOR = 1.1;
 
 const ELECTRON_UA_TOKEN = /\bElectron\/[^\s]+/g;
